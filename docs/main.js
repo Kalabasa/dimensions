ee6e8f202327ee11e51c02a0cb381bf83dcc7bea
Kalabasa <Kalabasa@users.noreply.github.com>
2019-07-29 21:28:40 +0800

    Trackingjs version

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
 var colorMap = {
-	dimensionsCool: [0x2d475e, 0x49ad9c, 0x99c89e],
+	dimensionsCool: [0x49ad9c, 0x99c89e],
 	dimensionsWarm: [0x4d2b52, 0xbb5b85],
 	dimensionsMid: [0xffd02d, 0xf8dd85],
 	dimensionsNeutral: [0xb8b9c9, 0xe8e8ee, 0xaaabb9, 0xcbced4]
@@ -19,8 +19,8 @@ function onDocumentLoad() {
 	snapshotCanvas = document.getElementById('snapshotCanvas');
 	canvas = document.getElementById('canvas');
 
-	snapshotCanvas.width = 100;
-	snapshotCanvas.height = 100;
+	snapshotCanvas.width = 128;
+	snapshotCanvas.height = 128;
 
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
@@ -109,7 +109,7 @@ function startTracking() {
 	trackingLoop();
 }
 
-function createColorFunction(colors, threshold = 17) {
+function createColorFunction(colors, threshold = 16) {
 	return function(sr, sg, sb) {
 		for (var color of colors) {
 			return computeColorDistance(color, (sr << 16) | (sg << 8) | sb) < threshold;
