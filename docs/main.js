c8800132163dfb306637011166efa5bbb42af21f
Kalabasa <Kalabasa@users.noreply.github.com>
2019-07-30 00:23:04 +0800

    Add threejs test

--- docs/main.js
+++ docs/main.js
@@ -1,29 +1,24 @@
-var colorMap = {
-	dimensionsCool: [0x49ad9c, 0x99c89e],
-	dimensionsWarm: [0x4d2b52, 0xbb5b85],
-	dimensionsMid: [0xffd02d, 0xf8dd85],
-	dimensionsNeutral: [0xb8b9c9, 0xe8e8ee, 0xaaabb9, 0xcbced4]
-};
-
 document.addEventListener('DOMContentLoaded', onDocumentLoad);
 
 var video;
-var canvas;
-var canvasContext;
+var scene;
+var camera;
+var controls;
+var renderer;
 
 function onDocumentLoad() {
 	video = document.getElementById('video');
-	canvas = document.getElementById('canvas');
-	canvasContext = canvas.getContext('2d');
 
 	var getUserMedia =
 		(navigator.mediaDevices &&
+			navigator.mediaDevices.getUserMedia &&
 			navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices)) ||
-		navigator.getUserMedia.bind(navigator) ||
-		navigator.webkitGetUserMedia.bind(navigator) ||
-		navigator.mozGetUserMedia.bind(navigator);
+		(navigator.getUserMedia && navigator.getUserMedia.bind(navigator)) ||
+		(navigator.webkitGetUserMedia &&
+			navigator.webkitGetUserMedia.bind(navigator)) ||
+		(navigator.mozGetUserMedia && navigator.mozGetUserMedia.bind(navigator));
 
-	if (getUserMedia) {
+	if (false && getUserMedia) {
 		try {
 			var stream = getUserMedia({
 				video: {
@@ -34,9 +29,46 @@ function onDocumentLoad() {
 				video.play();
 			});
 		} catch (error) {
-			alert('Something went wrong while getting the camera stream.');
+			alert(
+				'Something went wrong while getting the camera stream. Your experience may be limited.'
+			);
 		}
 	} else {
-		alert('Your browser does not support camera access.');
+		alert(
+			'Your browser does not support camera access. Your experience may be limited.'
+		);
 	}
+
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+
+	controls = new THREE.DeviceOrientationControls(camera);
+
+	var geometry = new THREE.BoxGeometry(10, 10, 10);
+	var material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+	var thing = new THREE.Mesh(geometry, material);
+	thing.rotation.x = 1;
+	thing.rotation.y = 1;
+	thing.position.y = -10;
+	thing.position.z = -5;
+	scene.add(thing);
+
+	renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0, 0);
+	renderer.domElement.style.position = 'absolute';
+	document.body.appendChild(renderer.domElement);
+
+	loop();
+}
+
+function loop() {
+	controls.update();
+	renderer.render(scene, camera);
+	requestAnimationFrame(loop);
 }
